e59c217709771f5abf832b39263158aa866f31ed
Felipe Quérette <felipe.querette@gmail.com>
2024-05-22 21:46:52 -0300

    Criada a classe ListaJogadores

---
+++ src/repositorios/ListaJogadores.java
@@ -0,0 +1,12 @@
+package src.repositorios;
+
+import src.entidades.Jogador;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaJogadores {
+
+    public static List<Jogador> jogadores = new ArrayList<>();
+
+}
